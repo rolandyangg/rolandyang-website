7534d22f5a6638ed3c0aa4943c1f05fa92a2640e
roland yang <rolandyang@gmail.com>
2022-12-25 01:50:27 -0500

    Homepage Progress

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,25 @@
 /**
  * @type {import('gatsby').GatsbyConfig}
  */
+
+// gatsby-config.js
+
 module.exports = {
-  plugins: [],
+  plugins: [
+    {
+      resolve: '@chakra-ui/gatsby-plugin',
+      options: {
+        /**
+         * @property {boolean} [resetCSS=true]
+         * if false, this plugin will not use `<CSSReset />
+         */
+        resetCSS: false,
+        /**
+         * @property {boolean} [isUsingColorMode=true]
+         * if false, this plugin will not use <ColorModeProvider />
+         */
+        isUsingColorMode: true,
+      },
+    },
+  ],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,52 @@
-import * as React from "react"
+import * as React from 'react'
+import { ChakraProvider, Flex, HStack, VStack, Stack, Box, Text, Heading, Image, Center, extendTheme, Button } from '@chakra-ui/react'
+
+const theme = extendTheme({
+  initialColorMode: 'dark',
+  useSystemColorMode: false,
+  colors: {
+		secondary: "#171a23",
+		primary: "#ffffff",
+	},
+	styles: {
+		global: () => ({
+			body: {
+				fontFamily:
+					"-apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif",
+				color: "white",
+				backgroundColor: "secondary",
+			},
+			"&::-webkit-scrollbar": {
+				width: "0.6em",
+			},
+			"&::-webkit-scrollbar-track": {
+				borderRadius: "0px",
+				background: "transparent",
+			},
+			"&::-webkit-scrollbar-thumb": {
+				background: "primary",
+				borderRadius: "50px",
+			},
+		}),
+	},
+})
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <ChakraProvider theme={theme}>
+      <Center p="50px" h="100vh">
+        <Stack direction={{ base: "column", md: "row"}} justify="center" flexWarp="wrap">
+          <Center>
+            <Image alt='Roland Yang' src="/hsgradpic.jpeg" borderRadius="lg" maxW="350px"/>
+          </Center>
+          <VStack mx="25px" align="left">
+            <Heading fontSize="5xl">hey 👋</Heading>
+            <Heading fontSize="4xl">my name is roland yang</Heading>
+            <Text>welcome to my website</Text>
+            <Text>feel free to explore</Text>
+          </VStack>
+        </Stack>
+      </Center>
+    </ChakraProvider>
+  );
 }
