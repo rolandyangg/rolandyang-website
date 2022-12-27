537085b8790e15c117bd68f7c7d3b8be9239dfb7
roland yang <rolandyang@gmail.com>
2022-12-26 19:56:30 -0500

    Hero Progress

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import { ChakraProvider, Flex, HStack, VStack, Stack, Box, Text, Heading, Image, Center, extendTheme, Button } from '@chakra-ui/react'
+import { ChakraProvider, Flex, HStack, VStack, Stack, Box, Link, Text, Heading, Image, Center, extendTheme, Spacer, Button } from '@chakra-ui/react'
 
 const theme = extendTheme({
   initialColorMode: 'dark',
@@ -31,21 +31,35 @@ const theme = extendTheme({
 	},
 })
 
+// https://chakra-ui.com/docs/components/flex
 export default function Home() {
   return (
     <ChakraProvider theme={theme}>
-      <Center p="50px" h="100vh">
-        <Stack direction={{ base: "column", md: "row"}} justify="center" flexWarp="wrap">
-          <Center>
+      <Center p="50px" justify="center" h="100vh">
+		<VStack>
+        <Flex direction={{ base: "column", md: "row"}} justify="center" align="center" flexWarp="wrap">
+          <Center my="25px" mx="30px">
             <Image alt='Roland Yang' src="/hsgradpic.jpeg" borderRadius="lg" maxW="350px"/>
           </Center>
-          <VStack mx="25px" align="left">
+          <VStack mx="30px" align="left" maxW="600px">
             <Heading fontSize="5xl">hey 👋</Heading>
-            <Heading fontSize="4xl">my name is roland yang</Heading>
-            <Text>welcome to my website</Text>
-            <Text>feel free to explore</Text>
-          </VStack>
-        </Stack>
+			<Heading fontSize="4xl">my name is <Text bgGradient='linear(to-l, #1ccbe2, #e96dff)'
+  bgClip='text' as="span" >roland yang</Text></Heading>
+            <Text fontSize="xl">welcome to my little website 💻</Text>
+            <Text fontSize="xl">i’m an undergraduate student studying computer science at ucla (go bwuins! 🐻). i like to code and do a bunch of other stuff 👨‍💻🥁🎥🏐🎮.</Text>
+			<HStack fontSize="lg" py="10px">
+				<Link href="mailto:rolandyang@ucla.edu">Email</Link>
+				<Link href="https://github.com/rolandyangg">GitHub</Link>
+				<Link href="https://www.linkedin.com/in/yangroland/">LinkedIn</Link>
+				<Link href="https://devpost.com/rolandyang">DevPost</Link>
+				<Link href="https://drive.google.com/file/d/1_IOzmp8a8JdewSncNE274h6pOH8LVXdL/view?usp=sharing">Resume/CV</Link>
+			</HStack>
+		  </VStack>
+        </Flex>
+		<Text justify="center" align="center">currently i am seeking internships in software engineering, however all opportunities are greatly appreciated!</Text>
+		<Text justify="center" align="center">feel free to reach out via any of my provided socials!
+</Text>
+		</VStack>
       </Center>
     </ChakraProvider>
   );
