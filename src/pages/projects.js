69755193429324118256aebf096aa5d5d128910b
whyroland <42717361+rolandyangg@users.noreply.github.com>
2023-01-02 01:47:00 -0500

    Projects Upload

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,11 +1,14 @@
 import * as React from 'react'
-import { ChakraProvider, Center, VStack, Heading, Flex } from "@chakra-ui/react";
+import { ChakraProvider, Center, Link, VStack, Heading, Flex, Icon } from "@chakra-ui/react";
+import { FaDev, FaGithub } from 'react-icons/fa'
 import theme from "../theme"
 import Navbar from '../components/navbar'
 import Footer from '../components/footer'
 import ProjectCard from '../components/projectcard'
 
-const projects = [
+const shuffle = arr => [...arr].sort(() => Math.random() - 0.5);
+
+let projects = [
     {
 		title: "Sorting Algorithm Visualizer",
 		link: "https://github.com/rolandyangg/sorting-algorithm-visualizer",
@@ -47,8 +50,45 @@ const projects = [
 		image: "tictactoegif.gif",
 		tech: ["Java", "Swing"],
 		award: null
-	}
+	},
+    {
+		title: "Trie CLI",
+		link: "https://github.com/rolandyangg/trie-system",
+		image: "trie_example.png",
+		tech: ["Javascript", "Node.js", "Express"],
+		award: null
+	},
+    {
+		title: "ProfessorAI",
+		link: "https://devpost.com/software/name-xazq3o",
+		image: "professoraisample.png",
+		tech: ["HTML/CSS", "Bootstrap", "Express", "Node.js", "Google Cloud Platform", "APIs", "NLP"],
+		award: "3rd Overall @ LingHacks III"
+	},
+    {
+		title: "CorgiCuisine",
+		link: "https://devpost.com/software/corgifeeds",
+		image: "corgicuisinesample.png",
+		tech: ["Arduino", "C++"],
+		award: "1st Overall @ MLH CorgiHacks"
+	},
+    {
+		title: "Personal Website",
+		link: "https://github.com/rolandyangg/rolandyang-website",
+		image: "personalwebsitesample.png",
+		tech: ["React", "Gatsby", "ChakraUI", "Netlify"],
+		award: null
+	},
+    {
+		title: "PDF Anonymizer",
+		link: "https://github.com/rolandyangg/pdf-anonymize",
+		image: "pdfanonymizersample.png",
+		tech: ["Python"],
+		award: null
+	},
 ]
+
+projects = shuffle(projects); 
 
 export default function Projects() {
     return (<>
@@ -57,7 +97,11 @@ export default function Projects() {
 
             <Center justify="center" p={{base: "25px", md: "100px"}}>
                 <VStack>
-                    <Heading mb="15px">projects</Heading>
+                    <Flex justify="space-between" align="center" flexWrap="wrap" w="90%" maxW="1200px">
+                        <Link href="https://github.com/rolandyangg" isExternal><Icon boxSize={8} as={FaGithub}/></Link>
+                        <Heading mb="15px" mx="10px">projects</Heading>
+                        <Link href="https://devpost.com/rolandyang" isExternal><Icon boxSize={8} as={FaDev}/></Link>
+                    </Flex>
                     <Flex justify="center" align="center" flexWrap="wrap" maxW="1200px">
                         {projects.map((project, i) => {
                             return <ProjectCard title={project.title} link={project.link} image={project.image} tech={project.tech} award={project.award} key={i}/>
